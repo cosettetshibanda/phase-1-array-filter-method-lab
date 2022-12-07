1955ee216079e0eb9239e360ded4ab3aeafcdb04
Cosette Tshibanda <cosettetshibanda@gmail.com>
2022-12-06 19:54:33 -0700

    commit tree 735e6b00c4babf0f4577ba87a440a356b955036c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+// function findMatching(drivers, string) {
+//     let filtered = drivers.filter(object => object.name === string) 
+//     if(filtered === undefined){
+//         return []
+//     }
+//     else{
+//         return string
+//     }
+
+// }
+// function fuzzyMatch(drivers, string){
+//     let firstLetter = drivers.filter()
+// }
+// function matchName(drivers, string){
+//     let nameOfElement = drivers.filter(object => object.hometown === string)
+//     if (nameOfElement === string){
+//         return nameOfElement.name
+//     }
+// }
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+  
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
